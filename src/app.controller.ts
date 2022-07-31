81c32cfa7b8a15d29119f542c7b8e4d391d5dd1e
Jonathan DSouza <j.dsouza@freetimecompany.nl>
2022-07-31 14:50:26 +0200

    added comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,14 +9,23 @@ export class AppController {
 	async getRenderedHTMLContent(@Req() req: Request, @Res() res: Response) {
 
 		const URL = req.query.url as string;
+
+		// Launch puppeteer instance
 		const browser = await puppeteer.launch();
 
+		// Create a new page 
 		const page = await browser.newPage();
+
+		// Disable caching to ensure we get the latest version of the page
 		await page.setCacheEnabled(false);
+
+		// Navigate to the URL
 		await page.goto(URL, {
 			waitUntil: 'networkidle2',
 			timeout: 0,
 		});
+
+		// Wait for the page to be loaded
 		const content = await page.content();
 
 		res.send(content);
